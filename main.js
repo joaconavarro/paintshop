cd13aa0f8643a1038274edd81f7aa9697a81065c
joaconavarro <106283185+joaconavarro@users.noreply.github.com>
2024-09-09 23:02:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const adminButton = document.getElementById('admin-panel-button');
 
     adminButton.addEventListener('click', () => {
-        window.location.href = '/admin/login.html';  // Redirects to the admin page
+        window.location.href = 'admin/login.html';  // Redirects to the admin page
     });
 });
