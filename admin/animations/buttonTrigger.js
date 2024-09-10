d18e01ac47e31d0c2cd23d36d8be014f5e43a5a3
joaconavarro <106283185+joaconavarro@users.noreply.github.com>
2024-09-10 12:01:46 -0300

    Update buttonTrigger.js

--- admin/animations/buttonTrigger.js
+++ admin/animations/buttonTrigger.js
@@ -1,19 +1,13 @@
-    function showAnimation(animationId) {
-        const animations = document.querySelectorAll('.animation');
-        animations.forEach(animation => {
-            if (animation.id === animationId) {
-                animation.classList.add('active');
-                if (animationId === 'animation2') {
-                    document.querySelector('.caja-1').classList.add('animate-caja-1');
+function showAnimation(animationId) {
+            const animations = document.querySelectorAll('.animation');
+            animations.forEach(animation => {
+                if (animation.id === animationId) {
+                    animation.classList.add('active');
+                } else {
+                    animation.classList.remove('active');
                 }
-            } else {
-                animation.classList.remove('active');
-                if (animation.id === 'animation2') {
-                    document.querySelector('.caja-1').classList.remove('animate-caja-1');
-                }
-            }
-        });
-    }
+            });
+        }
 
-    // Set default animation
-    showAnimation('animation1');
+        // Set default animation
+        showAnimation('animation1');
