a7059dd51bed85ac3f7fea63abb56a66c5584310
joaconavarro <106283185+joaconavarro@users.noreply.github.com>
2024-09-10 11:59:10 -0300

    Update buttonTrigger.js

--- admin/animations/buttonTrigger.js
+++ admin/animations/buttonTrigger.js
@@ -1,35 +1,19 @@
-    // Function to show the correct animation and trigger the animation for specific containers
     function showAnimation(animationId) {
         const animations = document.querySelectorAll('.animation');
-        
         animations.forEach(animation => {
             if (animation.id === animationId) {
                 animation.classList.add('active');
-                
-                // Add animation class to the caja-1 element if it exists
-                const cajaElement = animation.querySelector('.caja-1');
-                if (cajaElement) {
-                    cajaElement.classList.add('animate-caja-1');
+                if (animationId === 'animation2') {
+                    document.querySelector('.caja-1').classList.add('animate-caja-1');
                 }
             } else {
                 animation.classList.remove('active');
-                
-                // Remove animation class from the caja-1 element if it exists
-                const cajaElement = animation.querySelector('.caja-1');
-                if (cajaElement) {
-                    cajaElement.classList.remove('animate-caja-1');
+                if (animation.id === 'animation2') {
+                    document.querySelector('.caja-1').classList.remove('animate-caja-1');
                 }
             }
         });
     }
 
-    // Event listener for button clicks
-    document.querySelectorAll('button').forEach(button => {
-        button.addEventListener('click', (event) => {
-            const animationId = event.target.getAttribute('data-animation');
-            showAnimation(animationId);
-        });
-    });
-
-    // Set default animation if needed (for example, animation1)
+    // Set default animation
     showAnimation('animation1');
